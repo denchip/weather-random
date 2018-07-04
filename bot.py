f921927dc6f8aac2d5f7706e2aeb2f0e38f26cd1
Denis <taranov.denis.95@gmail.com>
2018-07-04 21:27:05 +0000

    added commands choice choiceN gennum

--- bot.py
+++ bot.py
@@ -7,13 +7,35 @@ bot = telebot.TeleBot(token)
 
 @bot.message_handler(commands=['choice'])
 def choice(message):
-    list = message.text.split(' ')[1:]
-    print(list)
-    bot.send_message(message.chat.id, 'pipka')
+    try:
+        list = message.text.split(' ')[1:]
+        bot.send_message(message.chat.id, list[random.randint(0, len(list)-1)])
+    except:
+        bot.send_message(message.chat.id, 'Ошибка ввода')
+
+@bot.message_handler(commands=['choiceN'])
+def choice(message):
+    try:
+        N = int(message.text.split(' ')[1])
+        list = message.text.split(' ')[2:]
+        random.shuffle(list)
+        bot.send_message(message.chat.id, str(list[:N]))
+    except:
+        bot.send_message(message.chat.id, 'Ошибка ввода')
+
+@bot.message_handler(commands=['gennum'])
+def choice(message):
+    try:
+        a = int(message.text.split(' ')[1])
+        b = int(message.text.split(' ')[2])
+        bot.send_message(message.chat.id, str(random.randint(a, b)))
+    except:
+        bot.send_message(message.chat.id, 'Ошибка ввода')
+
 
 @bot.message_handler(content_types=["text"])
-def repeat_all_messages(message): # Название функции не играет никакой роли, в принципе
-    bot.send_message(message.chat.id, message.text)
+def repeat_all_messages(message):
+    bot.send_message(message.chat.id, 'Я пока не могу общаться, используй команды')
 
 if __name__ == '__main__':
     bot.polling(none_stop=True)
