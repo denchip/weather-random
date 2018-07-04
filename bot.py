6d7d0188389147168a9ca472d6cc686a5a9c5256
Denis <taranov.denis.95@gmail.com>
2018-07-05 00:06:21 +0300

    bot

--- bot.py
+++ bot.py
@@ -1,8 +1,15 @@
 import telebot
+import random
 
 token = "465922976:AAGN6DmGRkxXZ6330eJrbuhvNvGmnTGZhUo"
 
 bot = telebot.TeleBot(token)
+
+@bot.message_handler(commands=['choice'])
+def choice(message):
+    list = message.text.split(' ')[1:]
+    print(list)
+    bot.send_message(message.chat.id, 'pipka')
 
 @bot.message_handler(content_types=["text"])
 def repeat_all_messages(message): # Название функции не играет никакой роли, в принципе
