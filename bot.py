254179ef458dbf6a753ef4ff11a5b67a120be774
Denis <taranov.denis.95@gmail.com>
2018-07-05 05:24:11 +0300

    webhook config

--- bot.py
+++ bot.py
@@ -3,8 +3,17 @@ import random
 import config
 
 bot = telebot.TeleBot(config.token)
+updater.start_webhook(listen='0.0.0.0',
+                      port=config.webhook_port,
+                      url_path=config.token,
+                      key=config.ssl_cert_pkey,
+                      cert=config.ssl_cert_path,
+                      webhook_url='https://{}:{}/{}'.format(config.server_adress, 
+                                                            config.webhook_port, 
+                                                            config.token))
 
-@bot.message_handler(commands=['choice'])
+
+@bot.message_handler(func=lambda message: True, commands=['choice'])
 def choice(message):
     try:
         list = message.text.split(' ')[1:]
@@ -12,7 +21,7 @@ def choice(message):
     except:
         bot.send_message(message.chat.id, 'Ошибка ввода')
 
-@bot.message_handler(commands=['choiceN'])
+@bot.message_handler(func=lambda message: True, commands=['choiceN'])
 def choice(message):
     try:
         N = int(message.text.split(' ')[1])
@@ -22,7 +31,7 @@ def choice(message):
     except:
         bot.send_message(message.chat.id, 'Ошибка ввода')
 
-@bot.message_handler(commands=['gennum'])
+@bot.message_handler(func=lambda message: True, commands=['gennum'])
 def choice(message):
     try:
         a = int(message.text.split(' ')[1])
@@ -32,9 +41,7 @@ def choice(message):
         bot.send_message(message.chat.id, 'Ошибка ввода')
 
 
-@bot.message_handler(content_types=["text"])
+@bot.message_handler(func=lambda message: True, content_types=["text"])
 def repeat_all_messages(message):
     bot.send_message(message.chat.id, 'Я пока не могу общаться, используй команды')
 
-if __name__ == '__main__':
-    bot.polling(none_stop=True)
