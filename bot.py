b10bab997b42b24361dc94ff5abc92801f568e8e
root <taranov.denis.95@gmail.com>
2018-07-06 09:08:15 +0000

    webhooks on cherrypy

--- bot.py
+++ bot.py
@@ -1,19 +1,59 @@
-import telebot
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# This is a simple echo bot using decorators and webhook with flask
+# It echoes any incoming text messages and does not use the polling method.
+
 import random
-import config.config
-
-bot = telebot.TeleBot(config.token)
-updater.start_webhook(listen='0.0.0.0',
-                      port=config.webhook_port,
-                      url_path=config.token,
-                      key=config.ssl_cert_pkey,
-                      cert=config.ssl_cert_path,
-                      webhook_url='https://{}:{}/{}'.format(config.server_adress, 
-                                                            config.webhook_port, 
-                                                            config.token))
+import cherrypy
+import telebot
+import logging
+import time
+import config.config as config
 
 
-@bot.message_handler(func=lambda message: True, commands=['choice'])
+API_TOKEN = config.token
+
+WEBHOOK_HOST = config.server_adress
+WEBHOOK_PORT = 8443  # 443, 80, 88 or 8443 (port need to be 'open')
+WEBHOOK_LISTEN = '0.0.0.0'  # In some VPS you may need to put here the IP addr
+
+WEBHOOK_SSL_CERT = config.ssl_cert_path  # Path to the ssl certificate
+WEBHOOK_SSL_PRIV = config.ssl_cert_pkey  # Path to the ssl private key
+
+# Quick'n'dirty SSL certificate generation:
+#
+# openssl genrsa -out webhook_pkey.pem 2048
+# openssl req -new -x509 -days 3650 -key webhook_pkey.pem -out webhook_cert.pem
+#
+# When asked for "Common Name (e.g. server FQDN or YOUR name)" you should reply
+# with the same value in you put in WEBHOOK_HOST
+
+WEBHOOK_URL_BASE = "https://%s:%s" % (WEBHOOK_HOST, WEBHOOK_PORT)
+WEBHOOK_URL_PATH = "/%s/" % (API_TOKEN)
+
+
+logger = telebot.logger
+telebot.logger.setLevel(logging.INFO)
+
+bot = telebot.TeleBot(API_TOKEN)
+
+# WebhookServer, process webhook calls
+class WebhookServer(object):
+    @cherrypy.expose
+    def index(self):
+        if 'content-length' in cherrypy.request.headers and \
+           'content-type' in cherrypy.request.headers and \
+           cherrypy.request.headers['content-type'] == 'application/json':
+            length = int(cherrypy.request.headers['content-length'])
+            json_string = cherrypy.request.body.read(length).decode("utf-8")
+            update = telebot.types.Update.de_json(json_string)
+            bot.process_new_updates([update])
+            return ''
+        else:
+            return 'HelloWorld'
+
+@bot.message_handler(commands=['choice'])
 def choice(message):
     try:
         list = message.text.split(' ')[1:]
@@ -21,7 +61,7 @@ def choice(message):
     except:
         bot.send_message(message.chat.id, 'Ошибка ввода')
 
-@bot.message_handler(func=lambda message: True, commands=['choiceN'])
+@bot.message_handler(commands=['choiceN'])
 def choice(message):
     try:
         N = int(message.text.split(' ')[1])
@@ -31,7 +71,7 @@ def choice(message):
     except:
         bot.send_message(message.chat.id, 'Ошибка ввода')
 
-@bot.message_handler(func=lambda message: True, commands=['gennum'])
+@bot.message_handler(commands=['gennum'])
 def choice(message):
     try:
         a = int(message.text.split(' ')[1])
@@ -45,3 +85,26 @@ def choice(message):
 def repeat_all_messages(message):
     bot.send_message(message.chat.id, 'Я пока не могу общаться, используй команды')
 
+
+# Remove webhook, it fails sometimes the set if there is a previous webhook
+bot.remove_webhook()
+
+time.sleep(0.1)
+
+# Set webhook
+bot.set_webhook(url=WEBHOOK_URL_BASE+WEBHOOK_URL_PATH,
+                certificate=open(WEBHOOK_SSL_CERT, 'r'))
+
+print('WEBHOOK_URL_BASE+WEBHOOK_URL_PATH')
+
+# Start cherrypy server
+cherrypy.config.update({
+    'server.socket_host': WEBHOOK_LISTEN,
+    'server.socket_port': WEBHOOK_PORT,
+    'server.ssl_module': 'builtin',
+    'server.ssl_certificate': WEBHOOK_SSL_CERT,
+    'server.ssl_private_key': WEBHOOK_SSL_PRIV
+})
+
+
+cherrypy.quickstart(WebhookServer(), WEBHOOK_URL_PATH, {'/': {}})
